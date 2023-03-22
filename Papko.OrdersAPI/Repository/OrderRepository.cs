7a8834f7cf61eeb96b3a558a9eb7861736f20938
Alexander Medved <medved1991alexander@gmail.com>
2023-03-22 21:13:46 +0300

    Add order repository

---
+++ Papko.OrdersAPI/Repository/OrderRepository.cs
@@ -0,0 +1,32 @@
+﻿using Papko.OrdersAPI.DTO;
+using Papko.OrdersAPI.Interfaces;
+
+namespace Papko.OrdersAPI.Repository;
+
+public sealed class OrderRepository : IRepository<Product>
+{
+    public void Add()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Product Find(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Product> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Product Remove(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Guid item)
+    {
+        throw new NotImplementedException();
+    }
+}
