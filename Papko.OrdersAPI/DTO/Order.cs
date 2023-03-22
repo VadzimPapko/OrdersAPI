93e7d9e5e6c5dfc6a6ccc64b69e7ee56f05e0bc3
Alexander Medved <medved1991alexander@gmail.com>
2023-03-22 20:35:18 +0300

    Add Order DTO

---
+++ Papko.OrdersAPI/DTO/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace Papko.OrdersAPI.DTO;
+
+public class Order
+{
+    public Guid Id { get; set; }
+    public decimal Total { get; set; }
+    public Guid BuyerId { get; set; }
+
+    public Order() => Id = Guid.NewGuid();
+}
