6ecc82d00e9efaae9615fd1d2ee242a30a04ddcd
Alexander Medved <medved1991alexander@gmail.com>
2023-03-22 20:30:39 +0300

    Created action methods

---
+++ Papko.OrdersAPI/Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Papko.OrdersAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        return default;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetOrderByID(Guid id)
+    {
+        return default;
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteOrderByID(Guid id)
+    {
+        return default;
+    }
+
+    [HttpPost("{id}")]
+    public async Task<IActionResult> CreateOrderByID(Guid id)
+    {
+        return default;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateOrderByID(Guid id)
+    {
+        return default;
+    }
+}
