cbfb01ad7cbacf26ac96e15e573e72c80a3e4a69
Alexander Medved <medved1991alexander@gmail.com>
2023-03-22 21:10:39 +0300

    Add interface repository

---
+++ Papko.OrdersAPI/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Papko.OrdersAPI.Interfaces;
+
+public interface IRepository<TModel> where TModel : class
+{
+    void Add();
+    IEnumerable<TModel> GetAll();
+    TModel Find(Guid id);
+    TModel Remove(Guid id);
+    void Update(Guid item);
+}
